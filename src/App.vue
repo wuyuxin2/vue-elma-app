25107c3eae472bef83549ac99539dd5c33341f15
wuyuxin <564791604@qq.com>
2019-06-29 15:19:49 +0800

    vue实战

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="header">
+      I am a header!
+    </div>
+    <div class="content">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import header from './components/header/header.vue';
+
+  export default {
+    components:{
+      header
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
